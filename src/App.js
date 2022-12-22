816c58537380feecabe9144b45acf657849be6d1
Prajwal-Mogaveera <prajwalmogaveera2000@gmail.com>
2022-12-22 22:35:33 +0530

    This is iNotebook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react"
+import Navbar from "./Components/Navbar"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import About from "./Components/About"
+import Home from "./Components/Home"
+import NoteState from "./Context/Notes/NoteState"
+import Alert from "./Components/Alert.js"
+import Login from "./Components/Login"
+import SignUp from "./Components/SignUp"
+import Profile from "./Components/Profile"
 
 function App() {
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (type, message) => {
+    setAlert({
+      type: type,
+      message: message
+    })
+
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <NoteState>
+      <Router>
+        <Navbar showAlert={showAlert} />
+        <Alert alert={alert} />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home showAlert={showAlert} />} />
+            <Route exact path="/profile" element={<Profile showAlert={showAlert} />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/login" element={<Login showAlert={showAlert} />} />
+            <Route exact path="/signup" element={<SignUp showAlert={showAlert} />} />
+          </Routes>
+        </div>
+      </Router>
+    </NoteState>
+  )
 }
 
-export default App;
+export default App
